046aee9e14edef716a5ff519e0c376fa58c9c1b3
Aries00101010 <kevin.henkes@laposte.net>
2017-01-10 12:17:00 +0100

    add littlePoney project

---
+++ littlePoney/src/src/Jockey.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Jockey {
+	String name;
+	int age;
+	String city;
+	String surname;
+	float weight;
+	float height;
+	int experience;
+}
---
+++ littlePoney/src/src/Poney.java
@@ -0,0 +1,7 @@
+package src;
+
+public class Poney {
+	String name;
+	int age;
+	String city;
+}
